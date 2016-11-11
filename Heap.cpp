7d3d41b53edcee63d4e69be13d1b622fe25818c1
KienHuynh <hckien@hcmut.edu.vn>
2016-11-11 16:56:11 +0700

    Fixed array2heap so that it does not allow duplicate entries

--- Heap.cpp
+++ Heap.cpp
@@ -50,15 +50,16 @@ Heap Heap::ArrayToHeap(int* arr, int length){
 	heap.heapPtr = new int[HEAP_MAX];
 	for (int i = 0; i < length; i++) {
 		if (!heap.DataExist(arr[i])) {
-			heap.heapPtr[i] = arr[i];
+			heap.size++;
+			heap.heapPtr[heap.size-1] = arr[i];
+			if (heap.size == HEAP_MAX) {
+				break;
+			}
 		}
 	}
-	for (int i = 0; i < length; i++) {
-		if (i == HEAP_MAX) {
-			break;
-		}
+
+	for (int i = 0; i < heap.size; i++) {
 		heap.ReHeapUp(i);
-		heap.size++;
 		heap.last++;
 	}
 	return heap;
@@ -158,4 +159,8 @@ bool Heap::DeleteHeap(int &dataOut) {
 		ReHeapDown(0);
 	}
 	return true;
+}
+
+int Heap::GetSize() {
+	return size;
 }
