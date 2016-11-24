2f5968a29fa34a0e307621214333b69e0b1398b6
Khang Nguyen <khang.neon.1997@gmail.com>
2016-11-24 21:07:10 +0700

    [FIX] Fix warning: using the result of an assignment as a condition without parentheses: AVLTree.cpp

--- AVLTree.cpp
+++ AVLTree.cpp
@@ -177,7 +177,7 @@ void AVLTree::DeleteLeftBalance(bool& shorter) {
 	}
 	else {
 		Node* leftTree = root->left;
-		if (leftTree->balance = RIGHT) {
+		if ((leftTree->balance = RIGHT)) {
 			Node* rightTree = leftTree->right;
 			if (rightTree->balance == RIGHT) {
 				leftTree->balance = LEFT;
@@ -284,7 +284,7 @@ bool AVLTree::AVLInsert(int newData) {
 	return true;
 }
 
-bool AVLTree::AVLInsert(Node* newPtr) 
+bool AVLTree::AVLInsert(Node* newPtr)
 {
 	if (newPtr == NULL) {
 		return false;
